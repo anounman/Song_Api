060369b97a76152f7a0a46fb466e991324691fea
anounboy <71139852+anounboy@users.noreply.github.com>
2021-06-25 17:29:59 +0530

    Add Error Return

--- app.py
+++ app.py
@@ -55,10 +55,11 @@ def video_link():
         hd_link = url_data["formats"][3]["url"]
         title = url_data["title"]
         responce = requests.get(download_link)
-        if  responce.status_code != "200":
-            return jsonify(error="This Song Is Not Downloadable.Please Try TO Search This Song With Another Name :(")
-        else:
+        if  responce:
             return jsonify(error="No Error",thumbnail=thumbnail , audio=(str(download_link)),hd_audio=str(hd_link))
+        else:
+            return jsonify(error="This Song Is Not Downloadable.Please Try TO Search This Song With Another Name :(")
+
      except Exception as e:
         print("ERROR==>"+str(e))
         return jsonify(error="Enter the name properly")
